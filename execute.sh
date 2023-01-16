2d48d12d6c5b401f6f8079f705b28de43892a622
Andres Navarro <contact@andresnav.com>
2023-01-16 18:25:45 +0100

    Added ability to reinstall the programs and mimelist

--- execute.sh
+++ execute.sh
@@ -2,11 +2,27 @@
 
 COPY_FOLDER="$HOME/.config/"
 CURRENT_FOLDER="$(pwd)"
+EXTRA_FILES_TO_LINK="mimeapps.list"
 
-folders=$(echo */)
+links="$(echo */ | sed 's/\///g') $EXTRA_FILES_TO_LINK"
 
-for i in $folders
+# Create syblinks to .config folder
+for i in $links
 do
-	# rm -rf "$COPY_FOLDER$i"
 	ln -fs "$CURRENT_FOLDER/$i" "$COPY_FOLDER"
 done
+
+# Reinstall all programs that must be compiled (dmw, dwmblocks) 
+if [ "$1" == "reinstall" ]; then
+	programs=( dwm dwmblocks )
+	
+	for i in "${programs[@]}"
+	do
+		echo "$i"
+		if [[ -d $i ]] && [[ -f $i/install.sh ]]; then
+			cd $i
+			./install.sh
+			cd $CURRENT_FOLDER
+		fi
+	done
+fi
