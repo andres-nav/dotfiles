9d5e722b109fdde33af2cf3bc8cf2aa192ee58ab
Andres Navarro <git@andresnav.com>
2023-05-27 12:46:20 +0200

    (fix) fixed nvidia driver

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -10,7 +10,14 @@ in {
 
   config = mkIf cfg.enable {
     hardware.nvidia = {
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
       modesetting.enable = true;
+      prime = {
+        offload.enable = true;
+
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
     };
     
     hardware.opengl = {
@@ -19,7 +26,7 @@ in {
       driSupport32Bit = true;
     };
 
-    services.xserver.videoDrivers = [ "intel" "nvidia" ];
+    services.xserver.videoDrivers = [ "nvidia" ];
 
     environment.systemPackages = with pkgs; [
       # Respect XDG conventions, damn it!
@@ -27,6 +34,15 @@ in {
         #!${stdenv.shell}
         mkdir -p "$XDG_CONFIG_HOME/nvidia"
         exec ${config.boot.kernelPackages.nvidia_x11.settings}/bin/nvidia-settings --config="$XDG_CONFIG_HOME/nvidia/settings"
+      '')
+
+      # To use nvidia offload, use nvidia graphics card for program nvidia-offload blender
+      (writeShellScriptBin "nvidia-offload" ''
+        export __NV_PRIME_RENDER_OFFLOAD=1
+        export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+        export __GLX_VENDOR_LIBRARY_NAME=nvidia
+        export __VK_LAYER_NV_optimus=NVIDIA_only
+        exec "$@"
       '')
     ];
   };
