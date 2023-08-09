03d31a59bebefe5317b55f23af9e527735033bfb
Andres Navarro <git@andresnav.com>
2023-08-09 17:45:06 +0200

    (dev) added other dependencies

--- packages/globaleaks/default.nix
+++ packages/globaleaks/default.nix
@@ -1,42 +1,73 @@
-{pkgs}:
-pkgs.stdenv.mkDerivation rec {
-  pname = "globaleaks";
-  version = "4.12.9";
-
-  src = pkgs.fetchurl {
-    url = "https://deb.globaleaks.org/bookworm/globaleaks_${version}_all.deb";
-    sha256 = "sha256-anjxasu6gFN+skcTU5ZEPZH2ngowdzKAxqTuSOxr2rg=";
+{
+  stdenv,
+  python3,
+  fetchurl,
+  fetchPypi,
+  autoPatchelfHook,
+  dpkg,
+  makeWrapper,
+}: let
+  mypython = python3.override {
+    packageOverrides = self: super: {
+      sqlalchemy = super.sqlalchemy.overridePythonAttrs (old: rec {
+        version = "1.4.46";
+        src = fetchPypi {
+          pname = "SQLAlchemy";
+          inherit version;
+          hash = "sha256-aRO4JH2KKS74MVFipRkx4rQM6RaB8bbxj2lwRSAMSjA=";
+        };
+        disabledTestPaths = [
+          "test/aaa_profiling"
+          "test/ext/mypy"
+        ];
+      });
+    };
   };
+in
+  stdenv.mkDerivation rec {
+    pname = "globaleaks";
+    version = "4.12.9";
 
-  nativeBuildInputs = with pkgs; [autoPatchelfHook dpkg makeWrapper];
+    src = fetchurl {
+      url = "https://deb.globaleaks.org/bookworm/globaleaks_${version}_all.deb";
+      sha256 = "sha256-anjxasu6gFN+skcTU5ZEPZH2ngowdzKAxqTuSOxr2rg=";
+    };
 
-  buildInputs = with pkgs; [
-    (python3.withPackages (ps:
-      with ps; [
-        twisted
-        h2
-        priority
-        sqlalchemy
-        pyotp
-        cryptography
-        pynacl
-        acme
-        python-gnupg
-        service-identity
-      ]))
-  ];
+    nativeBuildInputs = [autoPatchelfHook dpkg makeWrapper];
 
-  unpackPhase = "true";
+    buildInputs = [
+      (mypython.withPackages (ps:
+        with ps; [
+          twisted
+          h2
+          priority
+          sqlalchemy
+          pyotp
+          cryptography
+          pynacl
+          acme
+          python-gnupg
+          service-identity
+          debian
+          txtorcon
+        ]))
+    ];
 
-  installPhase = ''
-    mkdir -p $out/bin
-    dpkg -x $src $out
-    cp -r $out/usr/share/globaleaks/* $out/usr/lib/
+    unpackPhase = "true";
 
-    cat > $out/bin/globaleaks <<EOF
-    PYTHONPATH="$out/usr/lib/python3/dist-packages" $out/usr/bin/globaleaks
-    EOF
+    installPhase = ''
+      mkdir -p $out/bin
+      dpkg -x $src $out
 
-    chmod +x $out/bin/globaleaks
-  '';
-}
+      cat > $out/bin/globaleaks <<EOF
+      if [ ! -d "/tmp/globaleaks" ]; then
+        cp -r $out/usr/share/globaleaks /tmp
+        chmod +w /tmp/globaleaks
+      fi
+
+      PYTHONPATH="$out/usr/lib/python3/dist-packages" $out/usr/bin/globaleaks --working-path=/tmp/globaleaks -n --ip=localhost:8000
+      EOF
+
+      chmod +x $out/bin/globaleaks
+    '';
+  }
