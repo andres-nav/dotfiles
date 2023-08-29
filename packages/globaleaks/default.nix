c4fe1771f960b457937be18da94dd156a6b62306
Andres Navarro <git@andresnav.com>
2023-08-30 00:05:44 +0200

    (dev) globaleaks working

--- packages/globaleaks/default.nix
+++ packages/globaleaks/default.nix
@@ -4,6 +4,11 @@
   fetchPypi,
   python3,
   buildNpmPackage,
+  nodePackages,
+  pkg-config,
+  pixman,
+  cairo,
+  pango,
 }: let
   python = python3.override {
     packageOverrides = self: super: {
@@ -29,7 +34,7 @@
   };
 
   meta = with lib; {
-    description = "";
+    description = "GlobaLeaks is free, open source software enabling anyone to easily set up and maintain a secure whistleblowing platform. ";
     license = licenses.agpl3;
     platforms = platforms.linux;
     maintainers = with maintainers; [andresnav];
@@ -41,34 +46,48 @@
 
     sourceRoot = "${src.name}/client";
 
+    # Errro with conflicting versions of stylelint, using `15.10.2`.
     postPatch = ''
-      # substituteInPlace npm-shrinkwrap.json \
-      # --replace '"stylelint": "15.10.3",' '"stylelint": "15.10.2",'
+      substituteInPlace npm-shrinkwrap.json \
+      --replace '"stylelint": "15.10.3",' '"stylelint": "15.10.2",'
 
-      cp npm-shrinkwrap.json package-lock.json
+      substituteInPlace package.json \
+      --replace '"stylelint": "15.10.3",' '"stylelint": "15.10.2",'
+
+      ln -s npm-shrinkwrap.json package-lock.json
     '';
 
-    npmDepsHash = "sha256-n9Hcr3IeVOA3hAb2ZygVRTR8pJ3CliRjMTnw+PlweV4=";
-    # npmFlags = ["--legacy-peer-deps" "--ignore-scripts"];
+    nativeBuildInputs = [
+      nodePackages.node-gyp
+      python
+      pkg-config
+    ];
 
-    # makeCacheWritable = true;
-    # dontNpmInstall = true;
-    # dontNpmBuild = true;
+    buildInputs = [
+      pixman
+      cairo
+      pango
+    ];
+
+    env = {
+      CYPRESS_INSTALL_BINARY = 0; # cypress tries to download binaries otherwise
+    };
+
+    npmDepsHash = "sha256-GgPa9zIVqI/YQmO5xpi6qmabWpQOlpV2Du2TuKJPBVQ=";
+
+    buildPhase = ''
+      runHook preBuild
+
+      node_modules/grunt/bin/grunt build
+
+      runHook postBuild
+    '';
   };
 in
   python.pkgs.buildPythonApplication rec {
     inherit pname version src meta;
 
-    # outputs = [
-    #   "out"
-    #   "client"
-    # ];
-
     sourceRoot = "${src.name}/backend";
-
-    # nativeBuildInputs = [
-    #   nodejs
-    # ];
 
     propagatedBuildInputs = with python.pkgs; [
       twisted
@@ -86,14 +105,13 @@ in
       clientPackage
     ];
 
+    # Done so globaleaks can recognize where the client is built
+    postPatch = ''
+      substituteInPlace globaleaks/settings.py \
+      --replace '/usr/share/globaleaks/client/' '${clientPackage}/lib/node_modules/GlobaLeaks/build'
+    '';
+
     postInstall = ''
       find $out -name "__pycache__" -type d | xargs rm -rv
-
-      # mkdir -p $client
-
-      # cp -r $src/client $out/lib/python3.10 # FIXME: don't use python3.10 directory hardcoded
-
-      # cp -r $src/client/* $client
-      # cd $client && npm install -d && ./node_modules/grunt/bin/grunt build
     '';
   }
