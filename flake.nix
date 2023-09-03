b6b4161422bbd1be394215544aa06553ab910020
Andres Navarro <git@andresnav.com>
2023-09-03 22:18:21 +0200

    dev: init sops

--- flake.nix
+++ flake.nix
@@ -8,6 +8,11 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
